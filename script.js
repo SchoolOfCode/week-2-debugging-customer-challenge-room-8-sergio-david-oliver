6eac5ec2a0268de7e4b72427e86282667024d618
Sergio <sergeh96@gmail.com>
2024-12-04 14:41:10 +0000

    fixed issue #1

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ while (playAgain === false)
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   const userGuess = prompt("Guess a number between 1 and 10:");
 
-  if (userGuess = randomnumber) {
+  if (userGuess = randomNumber) {
     alert("Congratulations! You guessed the correct number.");
     score++;
   } else {
-    alert(`Sorry, the correct number was ${randomnumber}. You lose.`);
+    alert(`Sorry, the correct number was ${randomNumber}. You lose.`);
     score++;
   }
 
