4dad021aed129b8637320e72fffa07ff9beaa5b3
Sergio <sergeh96@gmail.com>
2024-12-04 14:45:28 +0000

    fixed issue #3

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let score;
 let playAgain = true;
 
-while (playAgain === false) {
+while (playAgain === true) {
   const randomNumber = Math.floor(Math.random() * 100) + 1;
   const userGuess = prompt("Guess a number between 1 and 10:");
 
